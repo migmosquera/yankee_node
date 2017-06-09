31799be2061a2cfd52270857cda30621836ad2f3
Oswaldo Teran <oswaldo_217@hotmail.com>
2017-06-09 12:40:59 -0400

    borrando el archivo cliente jade

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,36 +1,34 @@
 
 $(document).on("ready", init);
-    //Titulo de menu horizontal
-    $(document).ready( function() {
-      $('body').on("click", "ul li a", function(){
-        var title = $(this).data('title');
-        $('.brand-logo').children('h4').html(title);
-      });
-    });
-    //inicializando el menu vertical
-    $('.button-collapse').sideNav({
-      edge: 'left',
-      closeOnClick: true,
-      draggable: true
-        }
-    );
-    //inicializando registro de usuario
-    $('.button-add').sideNav({
-      menuWidth: 500,
-      edge: 'right',
-      closeOnClick: true,
-      draggable: true
-        }
-    );
-    //tamaño de la pantalla
-    $("#home-page").css("height", $(window).height() - parseInt($("#menu-horizontal").css("height").replace("px",'')));
-    $("#add-cliente").css("height", $(window).height() - parseInt($("#menu-horizontal").css("height").replace("px",'')));
-    //inicializando boton derecho del menu horizontal
-    $(".dropdown-button").dropdown();
-    //inicializando selects
-    $('select').material_select();
+//Titulo de menu horizontal
+$('body').on("click", "ul li a", function () {
+  var title = $(this).data('title');
+  $('.brand-logo').children('h4').html(title);
+});
+//inicializando el menu vertical
+$('.button-collapse').sideNav({
+  edge: 'left',
+  closeOnClick: true,
+  draggable: true
+}
+);
+//inicializando registro de usuario
+$('.button-add').sideNav({
+  menuWidth: 500,
+  edge: 'right',
+  closeOnClick: true,
+  draggable: true
+}
+);
+//tamaño de la pantalla
+$("#home-page").css("height", $(window).height() - parseInt($("#menu-horizontal").css("height").replace("px", '')));
+$("#add-cliente").css("height", $(window).height() - parseInt($("#menu-horizontal").css("height").replace("px", '')));
+//inicializando boton derecho del menu horizontal
+$(".dropdown-button").dropdown();
+//inicializando selects
+$('select').material_select();
 
 function init() {
-    
+
 }
 
